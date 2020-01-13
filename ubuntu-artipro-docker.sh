37f6dc56bac522c6ac6349ea14d0c1297a404a01
Joey <59530760+joeyfrog@users.noreply.github.com>
2020-01-13 14:17:31 +0200

    Create ubuntu-artipro-docker.sh

---
+++ ubuntu-artipro-docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Install docker, pull latest artifactory image and run it /w port forwarding
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+apt-cache policy docker-ce
+sudo apt-get install -y docker-ce
+sudo systemctl status docker
+sudo docker pull docker.bintray.io/jfrog/artifactory-pro:latest
+sudo docker run -d --name arti1 -p 8081:8081 docker.bintray.io/jfrog/artifactory-pro:latest
+sudo docker ps
