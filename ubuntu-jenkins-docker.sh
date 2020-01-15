8e1f668fe6cbbfc50f50ac67f5d1d95d3ba5a765
Joey <59530760+joeyfrog@users.noreply.github.com>
2020-01-15 12:58:13 +0200

    Update ubuntu-jenkins-docker.sh

--- ubuntu-jenkins-docker.sh
+++ ubuntu-jenkins-docker.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Install docker, pull latest jenkins image and run it /w port forwarding
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-#sudo apt-get update
-#apt-cache policy docker-ce
-#sudo apt-get install -y docker-ce
-#sudo systemctl status docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+apt-cache policy docker-ce
+sudo apt-get install -y docker-ce
+sudo systemctl status docker
 sudo docker pull jenkins
-sudo docker run -d --name jenk1 -p 8080:8080 jenkins
+sudo docker run -d --name jenk1 -p 8080:8080 -p 50000:50000 jenkins
 sudo docker ps
 echo "#### Jenkins initial admin pass: ###"
 sleep 15
