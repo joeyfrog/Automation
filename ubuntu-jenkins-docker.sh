03546493bcbdce0130469488c2c02ea6d7cbf57f
Joey <59530760+joeyfrog@users.noreply.github.com>
2020-02-03 17:43:52 +0200

    Update ubuntu-jenkins-docker.sh

--- ubuntu-jenkins-docker.sh
+++ ubuntu-jenkins-docker.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 # Install docker, pull latest jenkins image and run it /w port forwarding
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-apt-cache policy docker-ce
-sudo apt-get install -y docker-ce
+
+if ! docker ; then
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+     sudo apt-get update
+     apt-cache policy docker-ce
+     sudo apt-get install -y docker-ce
+fi
+
 sudo docker pull jenkins/jenkins
 sudo docker run -d --name jenk1 -p 8080:8080 -p 50000:50000 jenkins/jenkins
 sudo docker ps
