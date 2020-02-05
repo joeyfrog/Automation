254630be79b63603539d7fb2451e6d36b8ad993f
Joey <59530760+joeyfrog@users.noreply.github.com>
2020-02-05 11:00:08 +0200

    Update ubuntu-artipro.sh

--- ubuntu-artipro.sh
+++ ubuntu-artipro.sh
@@ -1,7 +1,7 @@
 # Install artifactory pro 6.17 & open jdk 8
 sudo apt update
 sudo apt install unzip -y
-sudo apt install openjdk-8-jdk
+sudo apt install openjdk-8-jdk -y
 java -version
 wget "https://bintray.com/jfrog/artifactory-pro/download_file?file_path=org%2Fartifactory%2Fpro%2Fjfrog-artifactory-pro%2F6.17.0%2Fjfrog-artifactory-pro-6.17.0.zip" -O arti.zip
 unzip arti.zip
