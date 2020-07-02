d50364a0c08c97f19b241f3d695e68fe6b69cc57
Joey <59530760+joeyfrog@users.noreply.github.com>
2020-07-02 16:38:02 +0300

    Create auto-deb.sh

---
+++ auto-deb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Create X different .deb packages
+# By joeyn@jfrog.com
+if which equivs-build >/dev/null; then
+  printf "How many deb packages?\n # "
+  read END
+  START=1
+  for (( c=$START; c<=$END; c++ ))
+  do
+    printf "building package number $c\n"
+    echo "Section: misc" > sourcefile
+    echo "Priority: optional" >> sourcefile
+    echo "Standards-Version: $c.0.0" >> sourcefile
+    echo "Package: debiantest$c" >> sourcefile
+    echo "Description: Hello world$c" >> sourcefile
+    equivs-build sourcefile > /dev/null 2>&1
+  done
+printf "Script done.\n"
+else
+    echo "Please install equivs"
+fi
